bf218d79e4ced5a1383aad624259d5ef0ee7ff48
dophatgl@gmail.com <dophatgl@gmail.com@7cd11aae-b1a9-2861-37e1-cb6f9ec0356c>
2014-08-24 07:16:39 +0000

    rap map.

--- trunk/GameCashtleVania/GameCashtleVania/HideObject.cpp
+++ trunk/GameCashtleVania/GameCashtleVania/HideObject.cpp
@@ -11,8 +11,8 @@ HideObject::HideObject(std::vector<std::string> arr)
 	this->_ID = atoi(arr.at(1).c_str());
 	this->_pos.x = atoi(arr.at(2).c_str());
 	this->_pos.y = atoi(arr.at(3).c_str());
-	this->_height = atoi(arr.at(4).c_str());
-	this->_width = atoi(arr.at(5).c_str());
+	this->_height = atoi(arr.at(5).c_str());
+	this->_width = atoi(arr.at(4).c_str());
 }
 
 void HideObject::setTypeHideObj(TypeHideObect _type)
