b65a800b1ba0eed5ccb96ea5a75aba1e8b4f74ea
sotuanhoang@gmail.com <sotuanhoang@gmail.com@7cd11aae-b1a9-2861-37e1-cb6f9ec0356c>
2014-08-24 15:12:06 +0000

    len cau thang. bao muot.

--- trunk/GameCashtleVania/GameCashtleVania/ManageGame.cpp
+++ trunk/GameCashtleVania/GameCashtleVania/ManageGame.cpp
@@ -196,7 +196,7 @@ void ManageGame::gameInit()
 	quadTreeObj = QuadTreeObject::getInstance();
 	
 	level = 1;
-	scene = 1;
+	scene = 2;
 	ManageAudio::getInstance()->playSound(TypeAudio::Stage_01_Vampire_Killer);
 
 	InfoScene* infoScene = MapLoader::getInstance()->getInfoSceneByKey(level * 10 + scene);
